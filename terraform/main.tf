9b19fe17eca56bcb82c64d98c908bd3861fd9e6a
Edson Limeme <edsonlimeme@capitecbank.co.za>
2025-06-13 14:32:41 +0200

    syntax update

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_policy" "resume_site" {
 resource "aws_s3_object" "website_files" {
   for_each = fileset("${path.module}/../resume-site", "**/*.*")
 
-  bucket       = edson-resume-site-demo
+  bucket       = "edson-resume-site-demo"
   key          = each.key
   source       = "${path.module}/../resume-site/${each.key}"
   content_type = lookup(var.mime_types, regex("\\.[^.]+$", each.key), "text/plain")
